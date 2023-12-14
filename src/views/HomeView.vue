abec332aaa3585662c492b1d8f75f0b9bf62b31e
Mouley-S <sivarajah.m@sierra.lk>
2023-12-14 12:59:19 +0530

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <div class="wrapper">
+      <h1>Vue Authenticate</h1>
+      <p>Vue Authenticate is a simple Vue.js authentication library for login/register and OAuth authentication.</p>
+      <p>It is built with Vue 3 and Vue Router.</p>
+      </div>
+  </main>
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+          <div>   
+           <h1>Login</h1> 
+          </div>
+
+    </div>
+</template>
+<script setup>
+// export default {
+//     name: 'LoginView',
+//     data () {
+//         return {
+//             user: {
+//                 email: '',
+//                 password: ''
+//             }
+//         }
+//     },
+//     methods: {
+//         login () {
+//             this.$auth.login({
+//                 params: {
+//                     email: this.user.email,
+//                     password: this.user.password
+//                 },
+//                 success: () => {
+//                     this.$router.push({name: 'home'})
+//                 },
+//                 error: (err) => {
+//                     console.log(err)
+//                 }
+//             })
+//         }
+//     }
+// }
+</script>
